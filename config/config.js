c908b5ffeae1293615b7bd71debc77d062b576dd
VaibhavRai01 <122098758+VaibhavRai01@users.noreply.github.com>
2023-12-18 13:23:36 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser = 'cysitebro@gmail.com';
+const emailPassword = 'kxfmhytsiouzlyqt';
+
+module.exports = {
+    sessionSecret,
+    emailPassword,
+    emailUser
+}
